fdeb1e2e8566a19eb1f13565b7338a45f30f31b3
ChristopherJohnAlappatt <ajchristopher2001@gmail.com>
2022-05-05 12:29:34 +0530

    completed BFS prac

--- dfs/dfs.py
+++ dfs/dfs.py
@@ -15,13 +15,19 @@ def dfs(visited, stack, output, graph, start_node):
     print("WELCOME TO DFS PROGRAM")
     stack.append(start_node)
     visited.append(start_node)
-
+    iteration = 0
     while stack:
+        iteration = iteration + 1
+        print("<------------------------ITERATION", iteration, "------------------------>")
+        print("Stack        => ", stack)
         m = stack.pop()
+        print("Pop ", m, "from the stack and mark it as visited")
+        print("Visited      => ", visited)
         output.append(m)
+        print("Output       => ", output)
 
         for neighbour in graph[m]:
-            if neighbour not in visited :
+            if neighbour not in visited:
                 visited.append(neighbour)
                 stack.append(neighbour)
 
